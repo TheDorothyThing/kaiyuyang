158a9175109ea2b4704e418745db1e30e2d105e0
TheDorothyThing <kaiyuyang.design@gmail.com>
2021-10-24 22:35:33 +0800

    ＊project kaomojiru linked, password function attempt

---
+++ kaiyuyang.js
@@ -0,0 +1,60 @@
+//Page onload animation
+
+function pageOnLoadAnimation() {
+    document.body.animate(
+        [
+            { opacity: "0%" },
+            { opacity: "100%" }
+        ], 1500,
+    )
+}
+
+document.body.addEventListener("load", pageOnLoadAnimation());
+
+//Passwords
+
+window.onload = function(){
+
+const passwords = ["Tree", "Notebook", "Bottledwater", "Camera", "Mask", "Ordinary", "Beautiful", "Popsicle", "Fan", "Alcohol", "Glassheart", "Sekret", "TheOrdinary", "Starbucks", "Hsinchu", "Comb", "SprayBottle", "PencilBox", "ApplePencil", "Sponge", "Nivea", "Muji", "ShavedIce", "PaperBag", "Lipstick", "Soap", "Coffee", "PotatoChips", "USBwire"];
+
+var userpassinput_1 = document.getElementById("project_passwordinput_1").value;
+var userpassinput_2 = document.getElementById("project_passwordinput_2").value;
+
+function checkPassword_1() {
+    if (userpassinput_1 === passwords[22]) {
+        window.location.replace("project_svc.html");
+    } else if (userpassinput_1 === ""||userpassinput_1 === null) {
+        document.getElementsByClassName("project_entrance_message_empty")[0].animate(passwordInputMessageKeyframes, passwordInputMessageOptions);
+    } else {
+        document.getElementsByClassName("project_entrance_message_error")[0].animate(passwordInputMessageKeyframes, passwordInputMessageOptions);
+    }
+}
+
+function checkPassword_2() {
+    if (userpassinput_2 === "123") {
+        document.getElementsByClassName("project_entrance_message_error")[1].animate(passwordInputMessageKeyframes, passwordInputMessageOptions);
+        //window.location.replace("project_sekretmeeting.html");
+    } else if (userpassinput_2 === ""||userpassinput_2 === null) {
+        document.getElementsByClassName("project_entrance_message_empty")[1].animate(passwordInputMessageKeyframes, passwordInputMessageOptions);
+    } else {
+        document.getElementsByClassName("project_entrance_message_error")[1].animate(passwordInputMessageKeyframes, passwordInputMessageOptions);
+    }
+}
+
+var passwordInputMessageKeyframes = 
+        [
+            { opacity: "0%", offset: 0},
+            { opacity: "100%", offset: 0.08},
+            { opacity: "100%", offset: 0.85},
+            { opacity: "0%", offset: 1}
+        ]
+
+var passwordInputMessageOptions = 
+{
+    duration: 1500,
+    easing: "ease-in-out"
+}
+
+document.getElementById("project_passwordbutton_1").addEventListener('click', checkPassword_1);
+document.getElementById("project_passwordbutton_2").addEventListener('click', checkPassword_2);
+}
